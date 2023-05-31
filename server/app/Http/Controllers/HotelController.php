ec6385e32facfa95ac549171b8172a39edf7fc0d
Huynt0811 <92984865+Huynt0811@users.noreply.github.com>
2023-05-31 23:04:41 +0700

    update

--- server/app/Http/Controllers/HotelController.php
+++ server/app/Http/Controllers/HotelController.php
@@ -6,16 +6,17 @@ use Illuminate\Http\Request;
 use App\Models\Hotels;
 use App\Traits\MessageStatusAPI;
 
+use function PHPUnit\Framework\isEmpty;
+
 class HotelController extends Controller
 {
     public function index(Request $request)
     {
         $request = Hotels::all();
-        if (empty($request) || $request = []) {
+
+        if ($request) {
             return MessageStatusAPI::notFound();
         }
-
-
         return $request;
     }
 }
