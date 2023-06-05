6b35eb70078b04da7a0c85f50d0c86d02e5c8977
quangvc <109118553+quangvc@users.noreply.github.com>
2023-06-05 18:18:34 +0700

    update forgot-password

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -86,69 +86,7 @@ class AuthController extends Controller
             : response()->json(['email' => __($status)]);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-    public function sendMail(Request $request)
-    {
-        $user = User::where('email', $request->email)->firstOrFail();
-        $passwordReset = PasswordReset::updateOrCreate([
-            'email' => $user->email,
-        ], [
-            'token' => Str::random(60),
-        ]);
-        if ($passwordReset) {
-            $user->notify(new ResetPasswordRequest($passwordReset->token));
-        }
-
-        return response()->json([
-            'message' => 'We have e-mailed your password reset link!'
-        ]);
-    }
-
-
-    public function reset(Request $request, $token)
-    {
-        $passwordReset = PasswordReset::where('token', $token)->firstOrFail();
-        if (Carbon::parse($passwordReset->updated_at)->addMinutes(720)->isPast()) {
-            $passwordReset->delete();
-
-            return response()->json([
-                'message' => 'This password reset token is invalid.',
-            ], 422);
-        }
-        $user = User::where('email', $passwordReset->email)->firstOrFail();
-        $updatePasswordUser = $user->update($request->only('password'));
-        $passwordReset->delete();
-
-        return response()->json([
-            'success' => $updatePasswordUser,
-        ]);
-    }
-
-
-
-
-
-    public function updatepass1(Request $request)
+    public function updatepass(Request $request)
     {
         $request->validate([
             'token' => 'required',
@@ -170,7 +108,9 @@ class AuthController extends Controller
         );
 
         return $status === Password::PASSWORD_RESET
-            ? redirect()->route('login')->with('status', __($status))
-            : back()->withErrors(['email' => [__($status)]]);
+            ? response()->json('status', __($status))
+            : response()->json(['email' => [__($status)]]);
     }
 }
+
+
