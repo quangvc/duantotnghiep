5a3b3e282d6d3f48a47261f8c5416e8f0b9d8dbd
Huynt0811 <92984865+Huynt0811@users.noreply.github.com>
2023-06-17 01:29:44 +0700

    comit

--- server/app/Http/Requests/HotelRequest.php
+++ server/app/Http/Requests/HotelRequest.php
@@ -9,6 +9,7 @@ class HotelRequest extends FormRequest
     public function rules()
     {
         return [
+<<<<<<< Updated upstream
             'hotel_name' => 'required|max:255',
             'hotel_phone' => 'required|max:20',
             'hotel_address' => 'required|max:255',
@@ -16,6 +17,41 @@ class HotelRequest extends FormRequest
             'star_rating' => 'required|integer|between:1,5',
             'region_id' => 'required|exists:tbl_regions,id',
             'status' => 'required|boolean',
+=======
+            'hotel_name' => 'required|string|max:255',
+            'hotel_phone' => 'required|string|max:20|min:8',
+            'hotel_address' => 'required|string|max:255',
+            'description' => 'nullable',
+            'star_rating' => 'required|numeric|between:1,5',
+            'region_id' => 'required|numeric|exists:tbl_regions,id',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'hotel_name.required' => 'Vui lòng không được bỏ trống !',
+            'hotel_name.string' => 'Nhập không đúng kiểu dữ liệu !',
+            'hotel_name.max' => 'Vui lòng không được nhập quá 255 ký tự !',
+
+            'hotel_phone.required' => 'Vui lòng không được bỏ trống !',
+            'hotel_phone.string' => 'Nhập không đúng kiểu dữ liệu !',
+            'hotel_phone.max' => 'Vui lòng không được nhập nhiều hơn 20 số !',
+            'hotel_phone.min' => 'Vui lòng không được nhập ít hơn 8 số !',
+
+            'hotel_address.required' => 'Vui lòng không được bỏ trống !',
+            'hotel_address.string' => 'Nhập không đúng kiểu dữ liệu !',
+            'hotel_address.max' => 'Vui lòng không được nhập quá 255 ký tự !',
+
+            'star_rating.required' => 'Vui lòng không được nhập quá 20 số !',
+            'star_rating.numeric' => 'Nhập không đúng kiểu dữ liệu !',
+
+            'region_id.required' => 'Vui lòng không được nhập quá 20 số !',
+            'region_id.numeric' => 'Nhập không đúng kiểu dữ liệu !',
+
+
+
+
+>>>>>>> Stashed changes
         ];
     }
 }
