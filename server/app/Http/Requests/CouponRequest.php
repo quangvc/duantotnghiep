b71679b5d4025e3888e9136f337d009a8445024f
quangnv199 <128734726+quangnv199@users.noreply.github.com>
2023-06-21 17:27:16 +0700

    update 21/06/2023

--- server/app/Http/Requests/CouponRequest.php
+++ server/app/Http/Requests/CouponRequest.php
@@ -14,21 +14,43 @@ class CouponRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|max:255',
-            'type' => 'required|max:255',
-            'value' => 'required|alpha_num',
-            'min' => 'alpha_num',
-            'max' => 'alpha_num',
-            'hotel_id' => 'alpha_num',
-            'quantity'=> 'alpha_num',
-            'dateStart'=>'date',
-            'dateEnd'=>'date',
+            'name' => 'bail|required|string|max:10',
+            'type' => 'bail|required|max:255',
+            'value' => 'bail|required|',
+            'min' => 'bail|required|max:2|numeric',
+            'max' => 'bail|required|numeric|max:2',
+            'hotel_id' => 'bail|required|alpha_num',
+            'quantity'=> 'bail|required|numeric|max:3',
+            'dateStart'=>'required|date',
+            'dateEnd'=>'required|date',
         ];
     }
     
     public function messages(){
         return [
-            'name.required'   => 'lỗi',
+            'name.required'   => 'Vui lòng nhập dữ liệu',
+            'name.max'   => 'Vui lòng nhập dưới 10 ký tự',
+
+            'type.required'   => 'Vui lòng nhập dữ liệu',
+            'type.max'   => 'Vui lòng nhập dưới 255 ký tự',
+
+            'value.required'   => 'Vui lòng nhập dữ liệu',
+
+            'min.required'   => 'Vui lòng nhập dữ liệu',
+            'min.numeric'   => 'Vui lòng nhập số',
+            'min.max'   => 'Vui lòng nhập dưới 2 ký tự',
+
+            'max.required'   => 'Vui lòng nhập dữ liệu',
+            'max.numeric'   => 'Vui lòng nhập số',
+            'max.max'   => 'Vui lòng nhập dưới 2 ký tự',
+
+            'hotel_id.required'   => 'Vui lòng nhập dữ liệu',
+
+            'quantity.required'   => 'Vui lòng nhập dữ liệu',
+
+            'dateStart.required'   => 'Vui lòng nhập dữ liệu',
+
+            'dateEnd.required'   => 'Vui lòng nhập dữ liệu',
            ];
     }
 
