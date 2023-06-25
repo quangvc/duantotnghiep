bdaeab6094910c49feb7a2a8f910ace8e90e5dd9
quangvc <109118553+quangvc@users.noreply.github.com>
2023-06-26 04:31:11 +0700

    Banner + delete image file

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -10,6 +10,7 @@ use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
+use App\Http\Resources\API\UserResource;
 use Spatie\Permission\Models\Role;
 
 class AuthController extends Controller
