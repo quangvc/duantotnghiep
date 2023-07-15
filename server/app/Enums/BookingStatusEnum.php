0fc8958a9016c4031dca87036f73e430bc69fa98
quangvc <109118553+quangvc@users.noreply.github.com>
2023-07-15 23:37:07 +0700

    fix many things

--- server/app/Enums/BookingStatusEnum.php
+++ server/app/Enums/BookingStatusEnum.php
@@ -4,11 +4,11 @@ namespace App\Enums;
 
 final class BookingStatusEnum
 {
-    public const PENDING = 0;
-    public const RESERVED = 1;
-    public const CONFIRMED = 2;
-    public const COMPLETED = 3;
-    public const CANCELLED = 4;
+    public const PENDING = 0;   // đang đặt, chưa thanh toán
+    public const RESERVED = 1;  // đã thanh toán
+    public const CONFIRMED = 2; // admin xác nhận
+    public const COMPLETED = 3; // đã checkout
+    public const CANCELLED = 4; // hủy
 
     public static function arrEnums()
     {
