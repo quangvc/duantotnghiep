ea42294811471c5639faad1823c8e961dc375d4c
Le Duong <114868700+Duongld0511@users.noreply.github.com>
2023-07-17 23:20:49 +0700

    Create booking

--- client/base/src/app/main/main-global.routing.ts
+++ client/base/src/app/main/main-global.routing.ts
@@ -16,11 +16,11 @@ const routes: Routes = [
     { path: 'filter', loadChildren: () => import('./pages/filter-page/filter-page.module').then(m => m.FilterPageModule)},
     { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule)},
     {
-      path: 'booking',
-      redirectTo: 'booking/bookingform', // Chuyển hướng đến 'booking/review' khi người dùng truy cập '/booking'
+      path: 'payment',
+      redirectTo: 'payment/bookingform', // Chuyển hướng đến 'booking/review' khi người dùng truy cập '/booking'
       pathMatch: 'full',
     },
-    {path: 'booking', component: BookingFormComponent, children: [
+    {path: 'payment', component: BookingFormComponent, children: [
       { path: 'review', loadChildren: () => import('./pages/booking-form/review/review.module').then(m => m.ReviewModule)},
       { path: 'bookingform', loadChildren: () => import('./pages/booking-form/booking/booking.module').then(m => m.BookingModule)}
     ]},
