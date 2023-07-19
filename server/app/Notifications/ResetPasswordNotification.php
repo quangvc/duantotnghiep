26973008dbbad7b8bf556f6b8f4cd453a9f182b3
quangvc <109118553+quangvc@users.noreply.github.com>
2023-07-20 05:21:01 +0700

    Update ResetPasswordNotification.php

--- server/app/Notifications/ResetPasswordNotification.php
+++ server/app/Notifications/ResetPasswordNotification.php
@@ -40,17 +40,16 @@ class ResetPasswordNotification extends Notification
      */
     public function toMail($notifiable)
     {
-        $url = 'http://localhost:4300/resetPasssword?token=' . $this->token . '&&';
+        $url = 'http://localhost:4300/resetPasssword?token=' . $this->token;
 
         return (new MailMessage)
-            ->subject(__('email.forgotPassword.name') . ' - ' . config('app.name') . '.')
+            ->subject(__('Reset Password Notification') . ' - ' . config('app.name') . '.')
             ->greeting(__('Xin chào') . '!')
             ->line(__('Bạn đang nhận được email này vì chúng tôi đã nhận được yêu cầu đặt lại mật khẩu cho tài khoản của bạn.
             ') . '')
             ->action(__('Đổi lại mật khẩu') . '', $url)
-            ->line(__('Liên kết đặt lại mật khẩu này sẽ hết hạn trong 60 phút.
-
-            Nếu bạn không yêu cầu đặt lại mật khẩu, không cần hành động nào nữa.'));
+            ->line(__('Liên kết đặt lại mật khẩu này sẽ hết hạn trong 60 phút.'))
+            ->line(__('Nếu bạn không yêu cầu đặt lại mật khẩu, không cần hành động nào nữa.'));
     }
 
     /**
