99003c223d31b93255f1308dccc79716328b9ffd
quangvc <109118553+quangvc@users.noreply.github.com>
2023-07-28 23:32:56 +0700

    update booking client

--- server/app/Http/Controllers/API/Client/BookingClientController.php
+++ server/app/Http/Controllers/API/Client/BookingClientController.php
@@ -78,7 +78,7 @@ class BookingClientController extends Controller
             $count_booked_rooms = BookingDetail::join('tbl_bookings', 'tbl_bookings.id', '=', 'booking_id')
                 ->where([
                     ['room_type_id', $item['room_type_id']],
-                    ['tbl_bookings.status', 1],
+                    ['tbl_bookings.status', '<=', 1],
                 ])
                 ->where(function ($query) use ($checkin_date, $checkout_date) {
                     $query->where([
@@ -129,7 +129,7 @@ class BookingClientController extends Controller
 
     public function cancelBooking($id) {
         $booking = Booking::find($id);
-        if ($booking->checkin_date->diffInDays(now()) >= 2 ) {
+        if ($booking->checkin_date->diffInDays(now()) >= 7 ) {
             $booking->update([
                 'status' => BookingStatusEnum::CANCELLED
             ]);
