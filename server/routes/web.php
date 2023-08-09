ee6355aca772ea0f772a6a0fc6fd79ab1be78f6b
quangvc <109118553+quangvc@users.noreply.github.com>
2023-08-09 12:45:31 +0700

    Update web.php

--- server/routes/web.php
+++ server/routes/web.php
@@ -18,21 +18,21 @@ use App\Http\Controllers\PaymentController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/test', function () {
-    return view('payment');
-});
-Route::get('/payment-return', [PaymentController::class, 'paymentReturn']);
-Route::post('/vnpay-payment', [PaymentController::class, 'vnpay_payment'])->name('payment');
+// Route::get('/test', function () {
+//     return view('payment');
+// });
+// Route::get('/payment-return', [PaymentController::class, 'paymentReturn']);
+// Route::post('/vnpay-payment', [PaymentController::class, 'vnpay_payment'])->name('payment');
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('welcome');
 
-Route::get('/admin', function () {
-    return view('admin.index');
-})->name('admin.login');
+// Route::get('/admin', function () {
+//     return view('admin.index');
+// })->name('admin.login');
 
-Route::get('/admin/dashboard', function () {
-    return view('admin.dashboard');
-})->name('admin.dashboard');
+// Route::get('/admin/dashboard', function () {
+//     return view('admin.dashboard');
+// })->name('admin.dashboard');
 
